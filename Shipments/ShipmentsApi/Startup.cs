3855f842e38aaf9e56e88f6d4d47ad810630289a
Andrew Withers <andiwivs@gmail.com>
2017-01-28 15:44:36 +0000

    Add barebones web api project

---
+++ Shipments/ShipmentsApi/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Owin;
+using ShipmentsApi;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace ShipmentsApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+                        
+            WebApiConfig.Register(config);
+
+            app.UseCors(CorsOptions.AllowAll);
+            app.UseWebApi(config);
+        }
+    }
+}
